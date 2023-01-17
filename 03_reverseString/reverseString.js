30aadd151ebc326dc978a8f49ccbd162a02d1ff0
Kiporion <meho.mustafic@outlook.com>
2023-01-17 14:33:27 +0100

    finished 4 excercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newstr="";
+    for(let i=0; i<=string.length; i++){
+        newstr+=string.charAt(string.length-i);
+    }
+    return newstr;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (...args) {
+    const array = args[0];
+    const newArray = [];
+    array.forEach((item) => {
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    // and return that array
+    return newArray;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
