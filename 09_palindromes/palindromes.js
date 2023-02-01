a7b0c3fd934f8ead403e6a6fdb259ed3ce95d788
Kiporion <meho.mustafic@outlook.com>
2023-02-01 15:04:35 +0100

    finished Array manipulation exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let newStr = str.replace(/[.,\/#!$%\^&\*; :{}=\-_`~()]/g,"").toLowerCase();
+    let len = Math.floor(newStr.length / 2);
+    for (let i = 0; i < len; i++)
+      if (newStr[i] !== newStr[newStr.length - i - 1])
+        return false;
+    return true;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const x=parseInt(num);
+    if (x<0){
+        return "OOPS";
+    }
+    let num1=0, num2=1, fib=1;
+    for (let i=1; i<x; i++){
+        fib=num1+num2;
+        num1=num2;
+        num2=fib;
+    }
+    return fib;
 };
 
 // Do not edit below this line
